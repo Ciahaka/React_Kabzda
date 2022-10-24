9ae674c8453674b3ad760cbe30f76cf1b41a35cb
Ciahaka <wilddiver.by@gmail.com>
2022-10-24 21:19:13 +0300

    add Stars

---
+++ src/component/star/Stars.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './Stars.module.css'
+
+export const Stars = () => {
+  return (
+    <div className={s.container}>
+      <div>****</div>
+      <div>****</div>
+      <div>****</div>
+      <div>****</div>
+      <div>****</div>
+    </div>
+  );
+}
+
+
