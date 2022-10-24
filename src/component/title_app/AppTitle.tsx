357fd0cb84739854e49760e78f4c171c9d816963
Ciahaka <wilddiver.by@gmail.com>
2022-10-24 21:57:54 +0300

    add App_Title

---
+++ src/component/title_app/AppTitle.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './AppTitle.module.css'
+
+export const AppTitle = () => {
+  return (
+    <div className={s.container}>
+      <h3 className={s.h3}>This is a menu</h3>
+      <>This is s component</>
+    </div>
+  );
+}
+
+
