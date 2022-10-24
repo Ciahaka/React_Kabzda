c92a4e68d629449ed465dc545518541898b64ae5
Ciahaka <wilddiver.by@gmail.com>
2022-10-24 21:19:23 +0300

    add Rating

---
+++ src/component/rating/Rating.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './Rating.module.css'
+
+export const Rating = () => {
+  return (
+    <div className={s.container}>
+      <div>PUK</div>
+      <div>PUK</div>
+      <div>PUK</div>
+      <div>PUK</div>
+      <div>PUK</div>
+    </div>
+  );
+}
+
+
