cbb7e6de46bc59a29e28310ccb784fd0c109d7a5
Ciahaka <wilddiver.by@gmail.com>
2022-10-24 21:57:33 +0300

    change Accordion

--- src/component/accordion/Accordion.tsx
+++ src/component/accordion/Accordion.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import s from './Accordion.module.css'
-
+import {AccordionBody} from './accordion_body/AccordionBody';
+import {AccordionTitle} from './accordion_title/AccordionTitle';
 
 export const Accordion = () => {
   return (
     <div className={s.container}>
-      <h3 className={s.h3}>This is a menu</h3>
-      <ul className={s.ul}>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
+      <AccordionTitle/>
+      <AccordionBody/>
     </div>
   );
 }
