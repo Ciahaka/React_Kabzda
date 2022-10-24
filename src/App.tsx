89c91268534a8bd91c5787db6755845e073a41c8
Ciahaka <wilddiver.by@gmail.com>
2022-10-24 21:57:03 +0300

    change App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-
 import './App.css';
-import {Stars} from './component/star/Stars';
 import {Accordion} from './component/accordion/Accordion';
 import {Rating} from './component/rating/Rating';
+import {AppTitle} from './component/title_app/AppTitle';
+import {Stars} from './component/star/Stars';
 
 function App() {
   return (
     <div className="App">
       <Stars/>
       <Stars/>
+      <AppTitle/>
       <Accordion/>
       <Rating/>
     </div>
