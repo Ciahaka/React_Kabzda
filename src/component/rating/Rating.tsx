220f45be7531a7ec5ea74da90f917249bd641bf0
Ciahaka <wilddiver.by@gmail.com>
2022-10-24 23:20:50 +0300

    change Rating

--- src/component/rating/Rating.tsx
+++ src/component/rating/Rating.tsx
@@ -1,11 +1,58 @@
 import React from 'react';
 import s from './Rating.module.css'
+import {Puk, PukType} from './puk/Puk';
 
-export const Rating = () => {
+export type TypeRating ={
+  value:number
+}
+
+export const Rating = (props:TypeRating) => {
+  if (props.value===1) {
+    return (
+      <div className={s.container}>
+        <Puk selected={true}/>
+        <Puk selected={false}/>
+        <Puk selected={false}/>
+        <Puk selected={false}/>
+      </div>
+    );
+  }
+  if (props.value===2) {
+    return (
+      <div className={s.container}>
+        <Puk selected={true}/>
+        <Puk selected={true}/>
+        <Puk selected={false}/>
+        <Puk selected={false}/>
+      </div>
+    );
+  }
+  if (props.value===3) {
+    return (
+      <div className={s.container}>
+        <Puk selected={true}/>
+        <Puk selected={true}/>
+        <Puk selected={true}/>
+        <Puk selected={false}/>
+      </div>
+    );
+  }
+  if (props.value===4){
+    return (
+      <div className={s.container}>
+        <Puk selected={true}/>
+        <Puk selected={true}/>
+        <Puk selected={true}/>
+        <Puk selected={true}/>
+      </div>
+    );
+  }
   return (
     <div className={s.container}>
-      <div>PUK</div>
-      <div>PUK</div>
+      <Puk selected={false}/>
+      <Puk selected={false}/>
+      <Puk selected={false}/>
+      <Puk selected={false}/>
     </div>
   );
 }
