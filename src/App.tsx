4c51ed346e120bfcfe901c6217d660282d421f33
Ciahaka <wilddiver.by@gmail.com>
2022-10-24 21:18:54 +0300

    change App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import {Stars} from './component/star/Stars';
+import {Accordion} from './component/accordion/Accordion';
+import {Rating} from './component/rating/Rating';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stars/>
+      <Stars/>
+      <Accordion/>
+      <Rating/>
     </div>
   );
 }
