25afe6f34d6d441a3bc6d3811d7a2acb0f04f435
Ciahaka <wilddiver.by@gmail.com>
2022-10-24 21:58:26 +0300

    add Accordion_Title

---
+++ src/component/accordion/accordion_title/AccordionTitle.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './AccordionTitle.module.css'
+
+
+
+export const AccordionTitle = () => {
+  return (
+    <div className={s.container}>
+      <h2>Title for Accordion</h2>
+    </div>
+  );
+}
