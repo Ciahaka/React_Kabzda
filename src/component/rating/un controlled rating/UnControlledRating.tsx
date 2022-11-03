a4eccbd1846ab8b4c078873808e8ac0dc33f3d4a
Ciahaka <wilddiver.by@gmail.com>
2022-11-03 21:53:19 +0300

    add UnControlledRating

---
+++ src/component/rating/un controlled rating/UnControlledRating.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import s from './UnControlledRating.module.css'
+
+
+// export type TypeRating ={
+//   value:number
+// }
+
+export const UnControlledRating = () => {
+
+
+
+  let [value, setValue] = useState(0)
+
+  return (
+    <div className={s.container}>
+      <Puk selected={value > 0}/>
+      <button onClick={() => setValue(1)}>1</button>
+      <Puk selected={value > 1}/>
+      <button onClick={() => setValue(2)}>2</button>
+      <Puk selected={value > 2}/>
+      <button onClick={() => setValue(3)}>3</button>
+      <Puk selected={value > 3}/>
+      <button onClick={() => setValue(4)}>4</button>
+    </div>
+  );
+}
+
+export type PukType = {
+  selected: boolean
+}
+
+const Puk = (props:PukType) => {
+
+
+
+  if (props.selected === true) {
+    return <span><b>Puk</b> </span>;
+  } else {
+    return <span>Puk</span>;
+  }
+
+}
+
+
+
+
+
