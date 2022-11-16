68daea996b8405d507912a47bf4f96f187f73472
Ciahaka <wilddiver.by@gmail.com>
2022-11-16 21:47:39 +0300

    add CallbackAccordion

---
+++ src/component/08-kabzda_callback/callback accordion/CallbackAccordion.tsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import s from './CallbackAccordion.module.css'
+import of from '../../05_kabzda_on_of/OnOf.module.css'
+import on from '../../05_kabzda_on_of/OnOf.module.css'
+
+export type PropsAccordionType = {
+  title: string
+  // collapsed: boolean
+}
+
+export const CallbackAccordion = (props:PropsAccordionType) => {
+
+  let[collapsed, setCollapsed]= useState(true)
+
+  return (
+    <div className={s.container}>
+      <AccordionTitle title={props.title} onClick={()=> {setCollapsed(!collapsed)}}/>
+      {/*<button className={of.of} onClick={()=> {setCollapsed(!collapsed)}}>Поешь!</button>*/}
+      {/*<button className={on.on} onClick={()=>setCollapsed(true)}>No eat!</button>*/}
+      {!collapsed && <AccordionBody/>}
+    </div>
+  );
+
+
+}
+
+export type PropsAccordionTitle = {
+  title:string
+  onClick:()=>void
+}
+
+const AccordionTitle = (props:PropsAccordionTitle) => {
+  return (
+    <div className={s.title}>
+      <h2 className={s.colorText} onClick={()=>{props.onClick()}}>{props.title}</h2>
+    </div>
+  );
+}
+
+const AccordionBody = () => {
+  return (
+    <div className={s.container}>
+
+      <ul className={s.body}>
+        <li>A-A-A</li>
+        <li>Почему</li>
+        <li>Болит</li>
+        <li>Мозк!?</li>
+      </ul>
+    </div>
+  );
+}
