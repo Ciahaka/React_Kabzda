502b80b665b856fb0b8955ac2b9ca79439ba0df3
Ciahaka <wilddiver.by@gmail.com>
2022-11-17 17:14:47 +0300

    fix old component

--- src/component/08-kabzda_callback/callback controled rating/CallbackControledRating.tsx
+++ src/component/08-kabzda_callback/callback controled rating/CallbackControledRating.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import s from './CallbackControlledRating.module.css'
+
 import {Puk, PukType} from './Puk';
 
+export type RatingValueType = 0|1|2|3|4
+
 export type TypeRating ={
-  value:number
+  value: RatingValueType
+  onClick:(value: RatingValueType)=>void
 }
 
 export const CallbackControlledRating = (props:TypeRating) => {
   return (
       <div className={s.container}>
-        <Puk selected={props.value>0}/>
-        <Puk selected={props.value>1}/>
-        <Puk selected={props.value>2}/>
-        <Puk selected={props.value>3}/>
+        <Puk selected={props.value>0} onClick={props.onClick} value={1}/>
+        <Puk selected={props.value>1} onClick={props.onClick} value={2}/>
+        <Puk selected={props.value>2} onClick={props.onClick} value={3}/>
+        <Puk selected={props.value>3} onClick={props.onClick} value={4}/>
       </div>
     );
   }
--- src/component/08-kabzda_callback/callback controled rating/Puk.tsx
+++ src/component/08-kabzda_callback/callback controled rating/Puk.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import {RatingValueType} from './CallbackControledRating';
+import span from '../CallbackRating.module.css'
 export type PukType = {
   selected:boolean
+  onClick:(value: RatingValueType)=>void
+  value: RatingValueType
 }
 
 export const Puk = (props:PukType) => {
-  return <span>{props.selected ? <b>Puk</b> : 'Puk' }</span>
+  return <span className={span.span} onClick={()=>{props.onClick(props.value)}} >{props.selected ? <b>Puk</b> : 'Puk' }</span>
  // if (props.selected){
  //   return <span><b>Puk</b> </span>;
  // } else {
