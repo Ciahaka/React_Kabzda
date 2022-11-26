3a73e969e3304b66863e412c8a9ad50357281f0f
Ciahaka <wilddiver.by@gmail.com>
2022-11-26 23:58:53 +0300

    change CallbackControlledRating.stories.tsx

--- src/component/08-kabzda_callback/callback controled rating/CallbackControlledRating.stories.tsx
+++ src/component/08-kabzda_callback/callback controled rating/CallbackControlledRating.stories.tsx
@@ -8,10 +8,10 @@ export default {
 }
 
 export const PukEmpty = ()=> <CallbackControlledRating value={0} onClick={x => x}/>
-export const PukEmpty_1 = ()=> <CallbackControlledRating value={1} onClick={x => x}/>
-export const PukEmpty_2 = ()=> <CallbackControlledRating value={2} onClick={x => x}/>
-export const PukEmpty_3 = ()=> <CallbackControlledRating value={3} onClick={x => x}/>
-export const PukEmpty_4 = ()=> <CallbackControlledRating value={4} onClick={x => x}/>
+export const Puk_1 = ()=> <CallbackControlledRating value={1} onClick={x => x}/>
+export const Puk_2 = ()=> <CallbackControlledRating value={2} onClick={x => x}/>
+export const Puk_3 = ()=> <CallbackControlledRating value={3} onClick={x => x}/>
+export const Puk_4 = ()=> <CallbackControlledRating value={4} onClick={x => x}/>
 
 export const PukChange = () => {
  const [rating,setRating]=useState<RatingValueType>(3);
