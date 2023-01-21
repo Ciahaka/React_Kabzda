a1056cb94c41241b32a314d85475cf98533cc53e
Ciahaka <wilddiver.by@gmail.com>
2023-01-21 20:48:02 +0300

    add factorialis

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default {
   title:'useMemo'
 }
 export const Example = ()=>{
+ const [a, setA]= useState<number>(5)
+ const [b, setB]= useState<number>(5)
 
+  let resultA = 1
+  let resultB = 1
+
+  for (let i = 1; i <= a; i++) {
+    resultA = resultA * i
+  }
+  for (let i = 1; i <= b; i++) {
+    resultB = resultB * i
+  }
   return <>
 
+    <input value={a} onChange={(e)=>setA(+e.currentTarget.value)}/>
+    <input value={b} onChange={(e)=>setB(Number(e.currentTarget.value))}/>
+    <hr/>
+    <div>
+      result A: {resultA}
+    </div>
+    <div>
+      result B: {resultB}
+    </div>
   </>
 }
